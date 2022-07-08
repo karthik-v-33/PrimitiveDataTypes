8f26e2ecbf1da1eaa8efdfaa1027a760b1e6395a
Karthik <105358710+karthik-v-33@users.noreply.github.com>
2022-07-08 23:53:23 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,39 @@
+package com.GitHub_Updates;
+
+public class PrimitiveDataTypes {
+/*Primitive Data Types (8)
+ * Using primitive and variables we can represent information in a java program.
+ * Primitives are inbuilt data types that helps us to create variables.
+ * {
+ * 	byte
+ * 	short
+ * 	int
+ * 	long
+ * 	float
+ * 	double
+ * 	char
+ * 	boolean
+ * }
+ * *String is not a primitive data type.
+ */
+	
+	public static void main(String[] args) {
+		byte a=1;
+		short b=23;
+		int c=1;
+		long d=124353456;
+		float e=2.1f;
+		double f=2.24;
+		char g='a';
+		boolean h=true;
+		System.out.println("byte      =  "+a);
+		System.out.println("short     =  "+b);
+		System.out.println("int       =  "+c);
+		System.out.println("long      =  "+d);
+		System.out.println("float     =  "+e);
+		System.out.println("double    =  "+f);
+		System.out.println("char      =  "+g);
+		System.out.println("boolean   =  "+h);
+	}
+
+}
